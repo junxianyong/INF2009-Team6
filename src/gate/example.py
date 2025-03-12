4dbddddcd344458644dd3af649f332dbe490b386
ekiost <choonkeatling@icloud.com>
2025-03-12 21:50:30 +0800

    Remove unused placeholder functions from example.py
    Cleaned up the code by deleting unused and incomplete placeholder functions related to face and voice verification, intruder capture, and other redundant logic. This streamlines the module and improves maintainability by removing unnecessary code.

--- src/gate/example.py
+++ src/gate/example.py
@@ -1,59 +1,10 @@
 import time
 
-
-def face_detected():
-    """
-    TODO: This function is a infinte loop that waits for a face to be detected
-    """
-    while True:
-        if input("Face detected? (y/n)").lower() == "y":
-            return True
-
-
 def open_gate(id):
     """
     TODO: This function will open the gate
     """
     print(f"Gate {id} opened")
-
-
-def face_verified():
-    """
-    TODO: This function is a infinte loop that waits for a face to be verified (after few tries?),
-    it should return personnel ID if the face is verified, None otherwise
-    """
-    while True:
-        x = input("Face verified? (y/n)")
-        if x.lower() == "y":
-            return "Choon Keat"
-        else:
-            return None
-
-
-def face_verified_with_id(personnel_id):
-    """
-    TODO: This function is a infinte loop that waits for a face to be verified with personnel ID,
-    it should return True if the face is verified
-    """
-    while True:
-        x = input(f"Face verified with personnel ID {personnel_id}? (y/n)")
-        if x.lower() == "y":
-            return True
-        else:
-            return False
-
-
-def voice_verified(personnel_id):
-    """
-    TODO: This function is a infinte loop that waits for a voice to be verified with personnel ID,
-    it should return True if the voice is verified
-    """
-    while True:
-        x = input(f"Voice verified with personnel ID {personnel_id}? (y/n)")
-        if x.lower() == "y":
-            return True
-        else:
-            return False
 
 
 def personnel_passed():
@@ -85,15 +36,6 @@ def mantrap_scan():
             return True
         else:
             return False
-
-
-def capture_intruder():
-    """
-    TODO: This function will capture the intruder and return the image buffer
-    """
-    print("Intruder captured")
-    return "image_buffer"
-
 
 def alert_buzzer_and_led():
     """
